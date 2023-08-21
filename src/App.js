21f70e5cb2fdb354603e82b01192c2b8462bea63
Justnoones <yanw97841@gmail.com>
2023-08-21 11:48:25 +0630

    prev-state in counter-feature

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
-import Image from "./assets/logo192.png"
+import React from 'react'
+import { useState } from 'react'
 
 export default function App() {
-  let [name, setName] = useState("Wai Yan Ye Yint");
-  let changeName = () => {
-    setName("Aung Aung");
-    console.log(name); // Wai Yan Ye Yint
+  let [count, setCount] = useState(0);
+  let increment = () => {
+    // setCount(count + 1); the result will be 1 no matter how many line you write because it's async
+    // setCount(count + 1); the result will be 1 no matter how many line you write because it's async
+    // setCount(count + 1); the result will be 1 no matter how many line you write because it's async
+    // setCount(count + 1); the result will be 1 no matter how many line you write because it's async
+    // setCount(count + 1); the result will be 1 no matter how many line you write because it's async
+    setCount((prevState) => prevState + 1); // 0 + 1 = 1
+    setCount((prevState) => prevState + 1); // 1 + 1 = 2
+    setCount((prevState) => prevState + 1); // 2 + 1 = 3
+    setCount((prevState) => prevState + 1); // 3 + 1 = 4
+    setCount((prevState) => prevState + 1); // 4 + 1 = 5
   }
   return (
-    <div className='app'>
-      <h1>{name}</h1>
-      <button onClick={changeName}>Change Name</button>
-    </div>
-  );
+      <div>
+        <h1>Counter</h1>
+        <p>{count}</p>
+        <button onClick={increment}>Increment</button>
+      </div>
+    )
 }
