371d7dc57a8b449345b3bdb562ab2b03c494edbe
Justnoones <yanw97841@gmail.com>
2023-08-21 20:42:20 +0630

    portal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './index.css'
+import { createPortal } from 'react-dom'
 
 export default function Modal({children}) {
   return (
-    <div className='modal-backdrop'>
+    createPortal(
+      <div className='modal-backdrop'>
         <div className='modal'>
           {children}
         </div>
-    </div>
-  )
+      </div>,
+      document.getElementById("modal"))
+  );
 }
