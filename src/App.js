8f7d7702dadeaee18b924175bc8a116136eee69f
Justnoones <yanw97841@gmail.com>
2023-08-21 12:02:47 +0630

    key and function parameter on delete post

--- src/App.js
+++ src/App.js
@@ -2,24 +2,38 @@ import React from 'react'
 import { useState } from 'react'
 
 export default function App() {
-  let [count, setCount] = useState(0);
-  let increment = () => {
-    // setCount(count + 1); the result will be 1 no matter how many line you write because it's async
-    // setCount(count + 1); the result will be 1 no matter how many line you write because it's async
-    // setCount(count + 1); the result will be 1 no matter how many line you write because it's async
-    // setCount(count + 1); the result will be 1 no matter how many line you write because it's async
-    // setCount(count + 1); the result will be 1 no matter how many line you write because it's async
-    setCount((prevState) => prevState + 1); // 0 + 1 = 1
-    setCount((prevState) => prevState + 1); // 1 + 1 = 2
-    setCount((prevState) => prevState + 1); // 2 + 1 = 3
-    setCount((prevState) => prevState + 1); // 3 + 1 = 4
-    setCount((prevState) => prevState + 1); // 4 + 1 = 5
+  let [posts, setPosts] = useState([
+    {
+      id : 1,
+      title : "First Title"
+    },
+    {
+      id : 2,
+      title : "Second Title"
+    },
+    {
+      id : 3,
+      title : "Third Title"
+    },
+  ])
+  let deletePost = id => {
+    setPosts(prevState => prevState.filter(post => post.id != id));
   }
   return (
-      <div>
-        <h1>Counter</h1>
-        <p>{count}</p>
-        <button onClick={increment}>Increment</button>
-      </div>
-    )
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {/* "!!" returns bollean value */}
+        {/* true && console.log("hello world"); // hello world */}
+        {/* fakse && console.log("hello world"); // false */}
+        {!!posts.length && posts.map(post => (
+          <li key={post.id}>
+            {post.title}
+            <button onClick={() => deletePost(post.id)}>Delete</button>
+          </li>
+        ))}
+        {!posts.length && <p>No Posts Available</p>}
+      </ul>
+    </div>
+  )
 }
