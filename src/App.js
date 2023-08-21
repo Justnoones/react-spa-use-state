ab5fcc1843fe99a6707cf9f4f3f3402c04182a91
Justnoones <yanw97841@gmail.com>
2023-08-21 11:43:42 +0630

    name-change-using-use-state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from "./assets/logo192.png"
 
 export default function App() {
+  let [name, setName] = useState("Wai Yan Ye Yint");
+  let changeName = () => {
+    setName("Aung Aung");
+    console.log(name); // Wai Yan Ye Yint
+  }
   return (
-    <div>
-      {/* 1st way - public folder way */}
-      {/* from public */}
-
-      {/* relative path */}
-      <img src='/logo192.png' />
-      {/* absoulute path */}
-      <img src='http://localhost:3000/logo192.png' />
-
-      {/* 2nd way - import */}
-      {/* from src */}
-
-      <img src={Image} />
+    <div className='app'>
+      <h1>{name}</h1>
+      <button onClick={changeName}>Change Name</button>
     </div>
-  )
+  );
 }
