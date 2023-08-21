745f842c94134dcd280ebb9e31a7461a8c5d8306
Justnoones <yanw97841@gmail.com>
2023-08-21 11:37:54 +0630

    add-image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Image from "./assets/logo192.png"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      {/* 1st way - public folder way */}
+      {/* from public */}
 
-export default App;
+      {/* relative path */}
+      <img src='/logo192.png' />
+      {/* absoulute path */}
+      <img src='http://localhost:3000/logo192.png' />
+
+      {/* 2nd way - import */}
+      {/* from src */}
+
+      <img src={Image} />
+    </div>
+  )
+}
